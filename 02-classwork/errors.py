fabfb2babdbdd8a37b05aea6467f2b8764c2ec24
utami <utamid.kusumawati@gmail.com>
2019-06-19 00:56:44 -0400

    new

---
+++ 02-classwork/errors.py
@@ -0,0 +1,58 @@
+#
+# "It doesn't work"
+#
+# 1. The error message
+# 2. The code around where the error is happening
+# 3. Expected output
+# 4. Actual output
+# 5. What you've tried already
+
+# Error
+print("Hello world, so full of errors")
+
+# Error
+num = 4.55
+print(round(4) * int('5') + 3)
+
+# Error
+name = "Roberta"
+degres = 14
+print("Hello, my name is " + name + " and it is " + degrees + "out today")
+
+# Error
+name = "Smushface"
+print("I have a cat named", name)
+
+# Error
+age = 4
+if age < 18:
+    print("You aren't old enough to vote") 
+
+# Error
+print("One two three four")
+print("Five six seven eight")
+print("Nine ten eleven twelve")
+
+# Error
+pies = 9
+print("You have eaten" + pies + "today")
+
+# Error
+current_year = 2013
+travel = "future"
+cars = True
+animal_type = "lions"
+if current_year > 2017:
+    print("It's the future!")
+
+# Error
+print('4' + '4')
+
+# Error
+year = 2017
+if year >= 1607:
+    print("The American colonies existed")
+elif year >= 1776:
+    print("The United States exists")
+else:
+    print("America was just a dream")
---
+++ 02-classwork/simple.py
@@ -0,0 +1,86 @@
+# There is nothing in this file.
+print("Hello world")
+name = "Smush"
+name2 = "Stumpy"
+name3 = "Boy Abby"
+
+print(name)
+print(name2)
+print(name3)
+#making a list, instead of making three separate variables, you can make it as one
+names = ("Smush", "Stumpy", "Boy Abby")
+print (names)
+print(len(names)
+#len is the shortened version of length, we use to calculate the)
+#len will tell you how long a string is
+#len will tell you how long a list is
+# len will tell you how long a list is
+# len will tell you how long a string is
+print(len("Smushface"))
+number_of_cats = len(names)
+print(f"I have {number_of_cats} cats")
+#always use f for a series of string print(f"xx" )
+ages = (4, 50, 32, 13, 102)
+#how to count 
+print(f"We have(len(ages))people")
+print("The largest age is", max(ages))
+print("The smallest age is", min(ages))
+print("The total of all the ages is", sum (ages))
+import statistics
+ages = [4, 50, 32, 13, 102)
+statistics.median(ages)
+print("The median age is", statistics.median(ages))
+print("The mean age is", statistics.mean(ages))
+print("The variance of the age is", statistics.stdev(ages))
+print("Hello", names)
+for name in names:
+  print("Hello", names)
+#Using : always in condition
+
+#say hello to each of the ages?
+for age in ages:
+  print("Hello", age)
+
+#first element is named zero
+#"Give me the first thing in names"
+name = names[0]
+print(name)
+
+#"Give me the second thing in names"
+name = names [1]
+print (name)
+
+#if you are ___ years old, you were born in ____
+for age in ages:
+  year = 2019 - age
+  print(f"Hello {age}, you were born in {year}")
+
+print (ages [1])
+
+#index the number you needs to do
+name_smush = "Smush"
+age_smush = 12
+color_smush = "tabby"
+
+name_stumpy = "Stumpy"
+age_stumpy = 11
+color_stumpy = "white and tabby"
+
+smush = {
+  'name': 'Smush',
+  'age': 12,
+  'color': 'tabby'
+}
+#a dictionary, curly bracket is dictionary, square bracket is list
+print(smush['name'])
+print(stumpy['color'])
+
+print(smush.keys())
+print(smush.values()
+
+#mylist
+#len(mylist)
+#mylist.
+#I'm real man
+
+
